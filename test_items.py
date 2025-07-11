7fcde320db0efbbfce8ff7ad3e509fb4cc92ac3e
Anna Berdeneva <nyanwolly@yandex.ru>
2025-07-11 20:23:09 +0400

    Добавление проекта

---
+++ test_items.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from selenium.webdriver.support.ui import Select
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_button_add_to_basket(browser):
+    browser.get(link)
+
+    button_add = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//button[contains(@class, 'btn-add-to-basket')]"))
+    )
+
+    select = Select(browser.find_element(By.XPATH, "//select[contains(@name, 'language')]"))
+    print(f"\nВыбранное значение языка на странице: {select.first_selected_option.text}")
+
+    assert button_add.is_displayed(), "Кнопка добавления в корзину не отображается"
+
+
+    time.sleep(10)
